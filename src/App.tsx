48360ea1011f6d44687a7bca3c5da0f9d0d7304e
henson <hensonlao@gmail.com>
2023-05-23 20:35:50 +0800

    Added react-router-dom and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,47 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import WelcomePage from "./pages/WelcomePage";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+interface IProtectedRouteProps {
+  isAuthenticated: boolean;
+  redirectPath?: string;
+  children: React.ReactNode;
+}
+
+const ProtectedRoute = ({
+  isAuthenticated = false,
+  redirectPath = "/login",
+  children,
+}: IProtectedRouteProps) => {
+  if (!isAuthenticated) {
+    return <Navigate to={redirectPath} replace />;
+  }
+  return <>{children}</>;
+};
+
 export default function App() {
-  return <></>;
+  const { authenticated } = useContext(AuthContext);
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute isAuthenticated={authenticated}>
+              <WelcomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
