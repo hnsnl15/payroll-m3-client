190d334f3ce99365f1c1c9cac97bd7f239d1c621
henson <hensonlao@gmail.com>
2023-05-23 09:37:57 +0800

    Configure material-ui theming

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClientProvider, QueryClient } from "react-query";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
