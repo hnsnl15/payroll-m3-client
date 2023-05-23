2923119e507a6a16c3ba3a1c387385f9e91b59b9
henson <hensonlao@gmail.com>
2023-05-23 20:36:31 +0800

    Added AuthContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import theme from "./theme";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { QueryClientProvider, QueryClient } from "react-query";
+import AuthContextProvider from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <AuthContextProvider>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
