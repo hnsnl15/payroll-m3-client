eead13988c1b515c9278596d0488bb61832e8034
henson <hensonlao@gmail.com>
2023-05-23 09:14:43 +0800

    Applied JSDocs

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,38 +1,75 @@
-import { useMutation, useQueryClient, MutationKey } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import jwt_decode from "jwt-decode";
 import Cookies from "js-cookie";
 
 const TOKEN_COOKIE_NAME = "jwtToken";
 
+/**
+ * Saves the JWT token in a cookie.
+ * @param {string} token - The JWT token to be saved.
+ * @returns {void}
+ */
 const saveToken = (token: string): void => {
   Cookies.set(TOKEN_COOKIE_NAME, token, { expires: 7 });
 };
 
+/**
+ * Removes the JWT token from the cookie.
+ * @returns {void}
+ */
 const removeToken = (): void => {
   Cookies.remove(TOKEN_COOKIE_NAME);
 };
 
-export const useAuth = () => {
+/**
+ * Custom React Query hook for handling JWT token authentication.
+ * @returns {Object} - Auth methods and authentication status.
+ */
+export const useAuth = (): object => {
   const queryClient = useQueryClient();
 
+  /**
+   * Mutation for logging in and saving the JWT token.
+   */
   const loginMutation = useMutation({
-    mutationFn: async (token: string) => {
+    /**
+     * Function that performs the login mutation.
+     * @param {string} token - The JWT token to be saved.
+     * @returns {Promise<any>} - The decoded token after saving.
+     */
+    mutationFn: async (token: string): Promise<any> => {
       saveToken(token);
       return await jwt_decode(token);
     },
   });
 
-  const logoutMutation = useMutation<void, unknown, void, MutationKey>({
-    mutationFn: async () => {
+  /**
+   * Mutation for logging out and removing the JWT token.
+   */
+  const logoutMutation = useMutation({
+    /**
+     * Function that performs the logout mutation.
+     * @returns {Promise<void>}
+     */
+    mutationFn: async (): Promise<void> => {
       removeToken();
       queryClient.clear();
     },
   });
 
+  /**
+   * Logs in the user by saving the JWT token.
+   * @param {string} token - The JWT token to be saved.
+   * @returns {Promise<void>}
+   */
   const login = async (token: string): Promise<void> => {
     await loginMutation.mutateAsync(token);
   };
 
+  /**
+   * Logs out the user by removing the JWT token.
+   * @returns {Promise<void>}
+   */
   const logout = async (): Promise<void> => {
     await logoutMutation.mutateAsync();
   };
@@ -40,7 +77,11 @@ export const useAuth = () => {
   return {
     login,
     logout,
-    isAuthenticated: () => {
+    /**
+     * Checks if the user is authenticated.
+     * @returns {boolean} - True if the user is authenticated, false otherwise.
+     */
+    isAuthenticated: (): boolean => {
       return Cookies.get(TOKEN_COOKIE_NAME) !== undefined;
     },
   };
