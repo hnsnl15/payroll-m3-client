02f80a01763c1657292ce2643e0d30da52ec401b
henson <hensonlao@gmail.com>
2023-05-23 20:41:19 +0800

    Created WelcomePage.tsx

---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export default function WelcomePage() {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <Button variant="contained" color="primary">
+        Click me!
+      </Button>
+    </>
+  );
+}
