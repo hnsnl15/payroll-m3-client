c8b5f96a6725374a0626c477c875f8630754b60a
henson <hensonlao@gmail.com>
2023-05-26 11:09:59 +0800

    Fix submit handler

--- src/pages/EmployeeFormPage.tsx
+++ src/pages/EmployeeFormPage.tsx
@@ -1,8 +1,17 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
 import { IEmployee } from "..";
 import * as Yup from "yup";
-import { Grid, TextField } from "@mui/material";
+import {
+  Button,
+  Grid,
+  TextField,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { css } from "@emotion/css";
+import { useMutation } from "react-query";
+import { postEmployee } from "../api";
+import { useState } from "react";
 
 const initialValues: IEmployee = {
   lastName: "",
@@ -50,37 +59,29 @@ const employeeSchema = Yup.object({
   hourlyRate: Yup.number().required("Hourly rate is required"),
 });
 
-const handleSubmit = () => {};
-
 const StyledGridContainer = css`
   background: #fff;
-  max-width: 1000px;
+  max-width: 80vw;
+  margin: auto;
   padding: 0.5rem;
 `;
 
 const StyledGridItemContainer = css`
-  display: flex;
-  flex-direction: column;
-  height: 100px;
+  display: grid;
+  height: 120px;
   align-items: center;
-  // justify-content: center;
 `;
 
-const StyleFormContainer = css`
+const StyledFormContainer = css`
   display: flex;
   flex-direction: column;
-  margin-top: 20px;
-  padding: 0.5rem;
+  margin-top: 70px;
+  gap: 30px;
+  height: max-content;
+  width: max-content;
   justify-content: center;
-  // align-items: center;
-`;
-
-const StyledMainContainer = css`
-  min-height: 100vh;
-  width: 100vw;
-  background: yellow;
-  margin-top: 50px;
-  padding: 1rem 0.5rem;
+  align-items: center;
+  padding: 30px;
 `;
 
 const StyledErrorMessage = css`
@@ -88,173 +89,334 @@ const StyledErrorMessage = css`
 `;
 
 export default function EmployeeFormPage() {
+  const theme = useTheme();
+  const isPhoneSize = useMediaQuery(theme.breakpoints.down("sm"));
+  const startOrCenter = isPhoneSize ? "center" : "flex-start";
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (
+    values: IEmployee,
+    { resetForm }: FormikHelpers<IEmployee>
+  ) => {
+    await postEmployee(values);
+  };
+
   return (
-    <main className={StyledMainContainer}>
-      <h1>Create an employee</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={employeeSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form className={StyleFormContainer}>
-          {/* Name and birthday fields */}
-          <Grid
-            container
-            className={StyledGridContainer}
-            justifyContent="center"
-            spacing={1}
-          >
-            <Grid item xs={12} md={4} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                label="First name"
-                name="firstName"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="firstName" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} md={4} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                label="Last name"
-                name="lastName"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="lastName" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} md={4} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                type="date"
-                name="birthday"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="birthday" />
-                  </span>
-                }
-              />
-            </Grid>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={employeeSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={StyledFormContainer}>
+        {/* Name and birthday fields */}
+        <h1>Create Employee</h1>
+        <Grid
+          container
+          className={StyledGridContainer}
+          spacing={1}
+          justifyContent={startOrCenter}
+        >
+          <Grid item xs={10} md={4} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              label="First name"
+              name="firstName"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="firstName" />
+                </span>
+              }
+            />
           </Grid>
+          <Grid item xs={10} md={4} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              label="Last name"
+              name="lastName"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="lastName" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={4} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="date"
+              name="birthday"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="birthday" />
+                </span>
+              }
+            />
+          </Grid>
+        </Grid>
+        {/* Address and phone number fields */}
+        <Grid
+          container
+          className={StyledGridContainer}
+          spacing={1}
+          justifyContent={startOrCenter}
+        >
+          <Grid item xs={10} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              label="Address"
+              name="address"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              sx={{ maxWidth: "90%" }}
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="address" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              label="Phone number"
+              name="phoneNumber"
+              variant="outlined"
+              fullWidth
+              sx={{ maxWidth: "90%" }}
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="phoneNumber" />
+                </span>
+              }
+            />
+          </Grid>
+        </Grid>
+        {/* Government ids */}
+        <Grid
+          container
+          className={StyledGridContainer}
+          spacing={1}
+          justifyContent={startOrCenter}
+        >
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="sssNo"
+              label="SSS #"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="sssNo" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="pagibigNo"
+              label="Pagibig #"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="pagibigNo" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="philhealthNo"
+              label="Philhealth #"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="philhealthNo" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="tinNo"
+              label="TIN #"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="tinNo" />
+                </span>
+              }
+            />
+          </Grid>
+        </Grid>
+        {/* Status, position, immediate supervisor */}
+        <Grid
+          container
+          className={StyledGridContainer}
+          spacing={1}
+          justifyContent={startOrCenter}
+        >
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="status"
+              label="Status"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="status" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="position"
+              label="Position"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="position" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              name="immediateSupervisor"
+              label="Immediate Supervisor"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="immediateSupervisor" />
+                </span>
+              }
+            />
+          </Grid>
+        </Grid>
 
-          {/* Address and phone number fields */}
-          <Grid
-            container
-            className={StyledGridContainer}
-            justifyContent="center"
-            spacing={1}
-          >
-            <Grid item xs={12} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                label="Address"
-                name="address"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                sx={{ maxWidth: "89%" }}
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="address" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                label="Phone number"
-                name="phoneNumber"
-                variant="outlined"
-                fullWidth
-                sx={{ maxWidth: "89%" }}
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="phoneNumber" />
-                  </span>
-                }
-              />
-            </Grid>
+        {/* basicSalary, riceSubsidy, phoneAllowance, clothingAllowance,
+        grossSemiMonthlyRate, hourlyRate, */}
+        <Grid
+          container
+          className={StyledGridContainer}
+          spacing={1}
+          justifyContent={startOrCenter}
+        >
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="basicSalary"
+              label="Basic Salary"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="basicSalary" />
+                </span>
+              }
+            />
           </Grid>
-
-          {/* Government ids */}
-          <Grid
-            container
-            className={StyledGridContainer}
-            justifyContent="center"
-            spacing={1}
-          >
-            <Grid item xs={12} md={3} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                name="sssNo"
-                label="SSS #"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="sssNo" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} md={3} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                name="pagibigNo"
-                label="Pagibig #"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="pagibigNo" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} md={3} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                name="philhealthNo"
-                label="Philhealth #"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="philhealthNo" />
-                  </span>
-                }
-              />
-            </Grid>
-            <Grid item xs={12} md={3} className={StyledGridItemContainer}>
-              <Field
-                as={TextField}
-                name="tinNo"
-                label="TIN #"
-                variant="outlined"
-                margin="normal"
-                helperText={
-                  <span className={StyledErrorMessage}>
-                    <ErrorMessage name="tinNo" />
-                  </span>
-                }
-              />
-            </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="riceSubsidy"
+              label="Rice Subsidy"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="riceSubsidy" />
+                </span>
+              }
+            />
           </Grid>
-        </Form>
-      </Formik>
-    </main>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="phoneAllowance"
+              label="Phone Allowance"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="phoneAllowance" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="clothingAllowance"
+              label="Clothing Allowance"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="clothingAllowance" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="grossSemiMonthlyRate"
+              label="Gross semi-monthly Rate"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="grossSemiMonthlyRate" />
+                </span>
+              }
+            />
+          </Grid>
+          <Grid item xs={10} md={3} className={StyledGridItemContainer}>
+            <Field
+              as={TextField}
+              type="number"
+              name="hourlyRate"
+              label="Hourly Rate"
+              variant="outlined"
+              margin="normal"
+              helperText={
+                <span className={StyledErrorMessage}>
+                  <ErrorMessage name="hourlyRate" />
+                </span>
+              }
+            />
+          </Grid>
+        </Grid>
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Formik>
   );
 }
