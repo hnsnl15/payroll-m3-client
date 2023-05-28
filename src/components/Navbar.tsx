5063fc1366f76ec843dcbec65f62008d1abc0c1f
henson <hensonlao@gmail.com>
2023-05-28 12:27:53 +0800

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { AppBar, Toolbar, Typography, Avatar } from "@mui/material";
 import { css } from "@emotion/css";
+import { Link } from "react-router-dom";
+import { StyledWhiteText } from "../css";
 
-const navbarStyles = css`
+const NavbarStyles = css`
   background-color: #333;
 `;
 
-const logoStyles = css`
+const LogoStyles = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -13,12 +15,14 @@ const logoStyles = css`
 
 export default function Navbar() {
   return (
-    <AppBar position="fixed" className={navbarStyles}>
-      <Toolbar className={logoStyles}>
+    <AppBar position="fixed" className={NavbarStyles}>
+      <Toolbar className={LogoStyles}>
         <Typography variant="h6">
-          <span>My App</span>
+          <Link to="/" className={StyledWhiteText}>
+            <span>Motor PH</span>
+          </Link>
         </Typography>
-        <Avatar alt="Login Avatar" src="src\assets\avatar.jpg" />
+        <Avatar alt="Login Avatar" src="/src/assets/avatar.jpg" />
       </Toolbar>
     </AppBar>
   );
