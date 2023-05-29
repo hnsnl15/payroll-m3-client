7574af8ce5dfdb23b007076295ba7563f947301e
henson <hensonlao@gmail.com>
2023-05-29 16:20:08 +0800

    Created log-out button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
-import { AppBar, Toolbar, Typography, Avatar } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Avatar,
+  Box,
+  Button,
+} from "@mui/material";
 import { css } from "@emotion/css";
 import { Link } from "react-router-dom";
 import { StyledWhiteText } from "../css";
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { TOKEN_COOKIE_NAME } from "../hooks/useAuth";
 
 const NavbarStyles = css`
   background-color: #333;
@@ -14,6 +24,8 @@ const LogoStyles = css`
 `;
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <AppBar position="fixed" className={NavbarStyles}>
       <Toolbar className={LogoStyles}>
@@ -22,7 +34,39 @@ export default function Navbar() {
             <span>Motor PH</span>
           </Link>
         </Typography>
-        <Avatar alt="Login Avatar" src="/src/assets/avatar.jpg" />
+        <Button
+          onClick={() => setShowMenu(!showMenu)}
+          sx={{ position: "relative" }}
+        >
+          <Avatar alt="Login Avatar" src="/src/assets/avatar.jpg" />
+        </Button>
+        {showMenu && (
+          <Box
+            sx={{
+              width: "130px",
+              height: "100px",
+              bgcolor: "white",
+              position: "absolute",
+              bottom: "-110px",
+              right: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "10px",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={() => {
+                Cookies.remove(TOKEN_COOKIE_NAME);
+                window.location.reload();
+              }}
+            >
+              Log out
+            </Button>
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
